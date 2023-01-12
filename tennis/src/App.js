c5195e12b34c00d7fe2bf7d977b010dff562ce76
junhyungkwon <wnsgud8845@gmail.com>
2023-01-12 15:07:41 +0900

    rest api get방식 서버 통신

--- tennis/src/App.js
+++ tennis/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+
 
 function App() {
+  axios.get('https://openapi.gg.go.kr/PublicTennis?KEY=6218b81ea45140b9b6c531cd21b52257&Type=json', {
+    headers: {
+      "Content-Type" : "application/json"
+    }
+  }).then(function(response){
+    console.log(response.data);
+  });
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      
     </div>
   );
 }
