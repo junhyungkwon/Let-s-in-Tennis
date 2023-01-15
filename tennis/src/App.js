df77dcf78048f7c2e61ab81773f78e39e8f07bee
junhyungkwon <wnsgud8845@gmail.com>
2023-01-15 18:12:01 +0900

    useEffect 사용하여 api 호출

--- tennis/src/App.js
+++ tennis/src/App.js
@@ -1,3 +1,4 @@
+import react,{useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
@@ -5,20 +6,29 @@ import axios from 'axios';
 
 
 function App() {
-  axios.get('https://openapi.gg.go.kr/PublicTennis?KEY=6218b81ea45140b9b6c531cd21b52257&Type=json', {
-    headers: {
-      "Content-Type" : "application/json"
-    }
-  }).then(function(response){
-    console.log(response.data);
-  });
-  return (
-    
-    <div className="App">
-      
-      
-    </div>
-  );
+  useEffect(()=>{
+    axios.get('https://openapi.gg.go.kr/PublicTennis?KEY=6218b81ea45140b9b6c531cd21b52257&Type=json')
+    .then(res=> console.log(res.data)).catch(err => console.log(err))
+  },[])
+  
+ 
 }
 
 export default App;
+
+
+
+
+
+ //  axios.get('https://openapi.gg.go.kr/PublicTennis?KEY=6218b81ea45140b9b6c531cd21b52257&Type=json', {
+     
+  //  }).then(function(response){
+  //    console.log(response.data);
+  //  });
+  // return (
+    
+  //   <div className="App">
+      
+      
+  //   </div>
+  // );
