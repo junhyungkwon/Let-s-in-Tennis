90ca5798348bd3c50a0c4280a9bf72510048cff1
junhyungkwon <aofry@naver.com>
2023-02-13 15:57:56 +0900

    fix: code 수정

--- tennis/src/App.js
+++ tennis/src/App.js
@@ -1,26 +1,15 @@
-import react,{useEffect, useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
-import Data from './Data.js';
-
-
-
-
+import react, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import Data from "./Data.js";
 
 function App() {
-  return(
-    
-    <div className='App'>
-      <Data/>
+  return (
+    <div className="App">
+      <Data />
     </div>
-  )
+  );
 }
-  
+
 export default App;
-
-
-
-
-
-
--- tennis/src/Data.js
+++ tennis/src/Data.js
@@ -1,54 +1,53 @@
-import react,{useEffect, useState, Component} from 'react';
-import './App.css';
-import axios from 'axios';
+import react, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function Data() {
-    const [users, setUsers] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-  
-    const fetchUsers = async () => {
-        // 요청이 시작 할 때에는 error 와 users 를 초기화하고
-        setError(null);
-        setUsers(null);
-        // loading 상태를 true 로 바꿉니다.
-        setLoading(true);
-        const response = await axios.get(
-          'https://openapi.gg.go.kr/PublicTennis?KEY=6218b81ea45140b9b6c531cd21b52257&Type=json'
-        ).then ((response)=>{
-          setLoading(false);
-          const data = response.data.PublicTennis.map(key =>
-            
-            {
-              console.log({key});
-              setUsers(key.row) 
-            }
-           )
-          console.log(response.data.PublicTennis)
+  const [users, setUsers] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchUsers = async () => {
+    // 요청이 시작 할 때에는 error 와 users 를 초기화하고
+    setError(null);
+    setUsers(null);
+    // loading 상태를 true 로 바꿉니다.
+    setLoading(true);
+    const response = await axios
+      .get(
+        "https://openapi.gg.go.kr/PublicTennis?KEY=6218b81ea45140b9b6c531cd21b52257&Type=json"
+      )
+      .then((response) => {
+        setLoading(false);
+        const data = response.data.PublicTennis.map((key) => {
+          console.log({ key });
+          setUsers(key.row);
         });
-      }
-  
-    useEffect(() => {
-      fetchUsers();
-    }, []);
-  
-    if (loading) return <div>로딩중..</div>;
-    if (error) return <div>에러가 발생했습니다</div>;
-    if (!users) return null;
-    return (
-      <div>  
-          <ul>
-        {users.map(user => (
+        console.log(response.data.PublicTennis);
+      });
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!users) return null;
+  return (
+    <div>
+      <ul>
+        {users.map((user) => (
           <li key={user}>
-            {user.SIGUN_NM} ({user.RM_MATR} {user.BOTM_MATRL_NM}{user.FACLT_NM})
+            {user.SIGUN_NM} ({user.RM_MATR} {user.BOTM_MATRL_NM}
+            {user.FACLT_NM})
           </li>
         ))}
       </ul>
-       
-        <button onClick={fetchUsers}>다시 불러오기</button>
-        </div>
-    );
-  }
-  
-  export default Data;
-  
+
+      <button onClick={fetchUsers}>다시 불러오기</button>
+    </div>
+  );
+}
+
+export default Data;
